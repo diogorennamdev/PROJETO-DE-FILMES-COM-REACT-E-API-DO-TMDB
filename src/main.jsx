ae04d4d96c4c695dfbc3f814b27423ca016e9bfe
diogorennam <diogorenan006@gmail.com>
2023-10-08 09:21:32 -0300

    routes: configuração do react-route para as páginas Movie, search e Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home/index.jsx';
+import Movie from './pages/Movie/index.jsx';
+import Search from './pages/Search/index.jsx';
+
+
+import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+          <Route path='movie/:id' element={<Movie />} />
+          <Route path='search' element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
